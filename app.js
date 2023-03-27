6ebc20393c34e52bed4a086faae24cc6f6894af8
Zach Taggart <ztaggart020701@gmail.com>
2023-03-27 17:06:18 -0400

    a8 backend

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+const app = express();
+app.use(express.json());
+app.use(cors());
+TuitsController(app);
+HelloController(app);
+UserController(app);
+app.listen(process.env.PORT || 4000);
