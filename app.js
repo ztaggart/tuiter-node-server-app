f22f61bc672948c7be9c8241522aa6270e411880
Zach Taggart <ztaggart020701@gmail.com>
2023-04-08 15:26:21 -0400

    a9

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/tuiter";
+mongoose.connect(CONNECTION_STRING);
+mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
 const app = express();
 app.use(express.json());
 app.use(cors());
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,30 +1,31 @@
-import posts from "./tuits.js";
-let tuits = posts;
+import * as tuitsDao from "./tuits-dao.js";
+//let tuits = posts;
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
   const newTuit = req.body;
   newTuit._id = new Date().getTime() + "";
   newTuit.likes = 0;
   newTuit.liked = false;
-  tuits.push(newTuit);
-  res.json(newTuit);
+  const insertedTuit = await tuitsDao.createTuit(newTuit);
+  res.json(insertedTuit);
 };
 
-const findTuits = (req, res) => res.json(tuits);
-const updateTuit = (req, res) => {
+const findTuits = async (req, res) => {
+  const tuits = await tuitsDao.findTuits();
+  console.log(tuits);
+  res.json(tuits);
+};
+const updateTuit = async (req, res) => {
   const tuitdIdToUpdate = req.params.tid;
   const updates = req.body;
-  const tuitIndex = tuits.findIndex((t) => t._id === tuitdIdToUpdate);
-  console.log(tuits[tuitIndex]);
-  tuits[tuitIndex] = { ...tuits[tuitIndex], ...updates };
-  console.log(tuits[tuitIndex]);
-  res.sendStatus(200);
+  const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
+  res.json(status);
 };
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
   const tuitdIdToDelete = req.params.tid;
-  tuits = tuits.filter((t) => t._id !== tuitdIdToDelete);
-  res.sendStatus(200);
+  const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+  res.json(status);
 };
 
 export default (app) => {
